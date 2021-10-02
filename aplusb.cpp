a708aa2a9e6b24993d20a44e2d6ef9c316bd1d3d
Guslik5 <guslik2003@mail.ru>
2021-10-02 18:29:14 +0300

    abcdf

--- aplusb.cpp
+++ aplusb.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 using namespace std;                  
-int main() {
+int main() 
+{
     int a, b, c;
     a=5;
     b=5;
     c=a+b;
+    cout << "я устал";
     cout << "Ответ: " << c;
 }
